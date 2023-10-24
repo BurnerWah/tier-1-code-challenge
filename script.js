c473e54e02ebcc548ce14264bcd88b7aa168fcd2
Burner <10004812+BurnerWah@users.noreply.github.com>
2023-10-23 19:09:12 -0500

    Added stuff to scrabble tiles

--- script.js
+++ script.js
@@ -22,6 +22,15 @@ console.log(myScrabbleTiles)
   { tile: 'D', score: 2 }
   { tile: 'A', score: 1 }
 */
+myScrabbleTiles.push(
+  { tile: "N", score: 1 },
+  { tile: "K", score: 5 },
+  { tile: "Z", score: 10 },
+  { tile: "X", score: 8 },
+  { tile: "D", score: 2 },
+  { tile: "A", score: 1 }
+)
+console.table(myScrabbleTiles)
 
 // 4. Remove the last tile from myScrabbleTiles and save it in a variable named removedTile.
 
